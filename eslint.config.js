2ff007bf554df352273e6fe03d7f7188847a9000
rizkyoktamara <rizky.oktamara26@gmail.com>
2026-02-13 15:17:23 +0700

    push eslint

--- eslint.config.js
+++ eslint.config.js
@@ -15,6 +15,9 @@ module.exports = [
       node: true, 
       jest: true 
     },
+    globals: {
+    __ENV: "readonly",
+    },
     rules: {
       "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
       "no-console": "off",
